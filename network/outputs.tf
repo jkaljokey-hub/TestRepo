244fe981fca6dbb5399555b17c48e51a20b43141
abubakar <jkaljokey@gmail.com>
2026-06-15 18:22:28 +0300

    welcome

---
+++ network/outputs.tf
@@ -0,0 +1,13 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
+output "subnet2_id" {
+  value = azurerm_subnet.subnet2.id
+}
+output "subnet3_id" {
+  value = azurerm_subnet.subnet3.id
+}
---
+++ network/variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {}
+variable "location" {}
+//vnet hub
+variable "vnet_name" {}
+variable "vnet_cidr" {}
+variable "subnet_name" {}
+variable "subnet_cidr" {}
+//vnet2 web spoke
+
+variable "vnet2_name" {}
+variable "vnet2_cidr" {}
+variable "subnet2_name" {}
+variable "subnet2_cidr" {}
+//vnet 3 proudct spoke
+variable "vnet3_name" {}
+variable "vnet3_cidr" {}
+variable "subnet3_name" {}
+variable "subnet3_cidr" {}
