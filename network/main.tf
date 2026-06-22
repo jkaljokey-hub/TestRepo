cacb4c9236684b4c0c0d7487342124e8c58d5791
abubakar <jkaljokey@gmail.com>
2026-06-22 13:21:07 +0300

    hello worlods

--- network/main.tf
+++ network/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = [var.vnet_cidr]
-  location            = var.location
+  location            = var.locatdsdsdion
   resource_group_name = azurerm_resource_group.rg.name
 }
 
